69c3efc215bc4ba543d3057f968f4f714433a850
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2024-01-25 16:31:44 -0500

    Cleared spacing issue in contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,108 +1,109 @@
-import "./contact.css";
-import Phone from "../../img/phone.png";
-import Email from "../../img/email.png";
-import Address from "../../img/address.png";
-import { useContext, useRef, useState } from "react";
-import { ThemeContext } from "../../context";
-import emailjs from "@emailjs/browser";
+  import "./contact.css";
+  import Phone from "../../img/phone.png";
+  import Email from "../../img/email.png";
+  import Address from "../../img/address.png";
+  import { useContext, useRef, useState } from "react";
+  import { ThemeContext } from "../../context";
+  import emailjs from "@emailjs/browser";
 
-const Contact = () => {
-  const formRef = useRef();
-  const [done, setDone] = useState(false);
-  const theme = useContext(ThemeContext);
-  const darkMode = theme.state.darkMode;
+  const Contact = () => {
+    const formRef = useRef();
+    const [done, setDone] = useState(false);
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-        formRef.current,
-        process.env.REACT_APP_EMAILJS_USER_ID
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setDone(true);
-        },
-        (error) => {
-          alert("Coudn't reach Rakshith! Try after some time");
-          console.log(error.text);
-        }
-      );
-  };
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      emailjs
+        .sendForm(
+          process.env.REACT_APP_EMAILJS_SERVICE_ID,
+          process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+          formRef.current,
+          process.env.REACT_APP_EMAILJS_USER_ID
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            setDone(true);
+          },
+          (error) => {
+            alert("Coudn't reach Rakshith! Try after some time");
+            console.log(error.text);
+          }
+        );
+    };
 
-  return (
-    <div className="c">
-      <div className="c-bg"></div>
-      <div className="c-wrapper">
-        <div className="c-left">
-          <h1 className="c-title">Let's Talk</h1>
-          <div className="c-info">
-            <div className="c-info-item">
-              <img src={Phone} alt="" className="c-icon" />
-              <a href="tel:+15149742519"> +1 (514)-974-2519</a>
-            </div>
-            <div className="c-info-item">
-              <img className="c-icon" src={Email} alt="" />
-              <a href="mailto:rakshithraj.gp@gmail.com">
-                rakshithraj.gp11@gmail.com
-              </a>
-            </div>
-            <div className="c-info-item">
-              <img className="c-icon" src={Address} alt="" />
-              Montreal, Quebec
+    return (
+      <div className="c">
+        <div className="c-bg"></div>
+        <div className="c-wrapper">
+          <div className="c-left">
+            <h1 className="c-title">Let's Talk</h1>
+            <div className="c-info">
+              <div className="c-info-item">
+                <img src={Phone} alt="" className="c-icon" />
+                <a href="tel:+15149742519"> +1 (514)-974-2519</a>
+              </div>
+              <div className="c-info-item">
+                <img className="c-icon" src={Email} alt="" />
+                <a href="mailto:rakshithraj.gp@gmail.com">
+                  rakshithraj.gp11@gmail.com
+                </a>
+              </div>
+              <div className="c-info-item">
+                <img className="c-icon" src={Address} alt="" />
+                Montreal, Quebec
+              </div>
             </div>
           </div>
-        </div>
-        <div className="c-right">
-          <p className="c-desc">
-            <b>Wanna get in touch?</b> and talk about the project
-          </p>
-          <form ref={formRef} onSubmit={handleSubmit}>
-            <input
-              style={{
-                backgroundColor: darkMode && "#333",
-                color: darkMode && "white",
-              }}
-              type="text"
-              placeholder="Name"
-              name="user_name"
-            />
-            <input
-              style={{
-                backgroundColor: darkMode && "#333",
-                color: darkMode && "white",
-              }}
-              type="text"
-              placeholder="Subject"
-              name="user_subject"
-            />
-            <input
-              style={{
-                backgroundColor: darkMode && "#333",
-                color: darkMode && "white",
-              }}
-              type="text"
-              placeholder="Email"
-              name="user_email"
-            />
-            <textarea
-              style={{
-                backgroundColor: darkMode && "#333",
-                color: darkMode && "white",
-              }}
-              rows="5"
-              placeholder="Message"
-              name="message"
-            />
-            {done ? "Thank you..." : <button disabled={done}>Submit</button>}
-          </form>
+          <div className="c-right">
+            <p className="c-desc">
+              <b>Wanna get in touch?</b> and talk about the project
+            </p>
+            <form ref={formRef} onSubmit={handleSubmit}>
+              <input
+                style={{
+                  backgroundColor: darkMode && "#333",
+                  color: darkMode && "white",
+                }}
+                type="text"
+                placeholder="Name"
+                name="user_name"
+              />
+              <input
+                style={{
+                  backgroundColor: darkMode && "#333",
+                  color: darkMode && "white",
+                  margin:'.1em'
+                }}
+                type="text"
+                placeholder="Subject"
+                name="user_subject"
+              />
+              <input
+                style={{
+                  backgroundColor: darkMode && "#333",
+                  color: darkMode && "white",
+                }}
+                type="text"
+                placeholder="Email"
+                name="user_email"
+              />
+              <textarea
+                style={{
+                  backgroundColor: darkMode && "#333",
+                  color: darkMode && "white",
+                }}
+                rows="5"
+                placeholder="Message"
+                name="message"
+              />
+              {done ? "Thank you..." : <button disabled={done}>Submit</button>}
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default Contact;
+  export default Contact;
