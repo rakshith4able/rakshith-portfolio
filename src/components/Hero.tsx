ca66a78e66743282b77d2fae78c0cbd174598d27
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-13 17:33:13 -0400

    Implement Hero Component with Spotlight from Aceternity

---
+++ src/components/Hero.tsx
@@ -0,0 +1,27 @@
+import { Spotlight } from "./ui/Spotlight";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.3] relative flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-linear-to-b from-neutral-200 to-neutral-500 py-8">
+          fsdfsd
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Hero;
