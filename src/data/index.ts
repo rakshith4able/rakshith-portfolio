74dd9e6c0ffc3838c2409f08daf2bd18e34acd6d
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-22 22:20:02 -0400

    Updated the content of the about grid

--- src/data/index.ts
+++ src/data/index.ts
@@ -78,7 +78,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently learning React Native with Expo",
+    title: "Currently working on a Project using React Native with Expo",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
